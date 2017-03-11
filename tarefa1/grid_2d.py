b1b5818b2adbd1eb8f03e784b831cb75faa7ae11
tomas <tomas.abril@gmail.com>
2017-03-11 00:34:23 -0300

    pdf; organizando em pasta

--- tarefa1/grid_2d.py
+++ tarefa1/grid_2d.py
@@ -1,7 +1,10 @@
-### Tomás Abril
+# Tomás Abril
 
+
+# funções -------------------------------------
 def print_grid():
     print('\n'.join(map(str, grid)))
+
 
 # criando ambiente  ----------------------------
 while 1:
@@ -10,7 +13,7 @@ while 1:
         coluna = int(input("quantas colunas: "))
         break
     except ValueError:
-         print("Não é um numero!")
+        print("Não é um numero!")
 
 agentPos = [0, 0]
 grid = [['_' for i in range(coluna)] for j in range(linha)]
@@ -21,17 +24,16 @@ while 1:
         print("posicao do obstaculo a adicionar")
         linhatmp = int(input("linha: "))
         colunatmp = int(input("coluna:"))
-        if 0<=linhatmp<linha and 0<=colunatmp<coluna:
+        if 0 <= linhatmp < linha and 0 <= colunatmp < coluna:
             grid[linhatmp][colunatmp] = '#'
     except ValueError:
-         print("Não é um numero!")
+        print("Não é um numero!")
     print_grid()
     try:
         if int(input("1 para sair da insercao de paredes ")) == 1:
             break
     except ValueError:
         pass
-
 
 print("posição inicial do agente")
 while 1:
@@ -57,61 +59,60 @@ while 1:
     elif movimento == 5:
         print("Posição atual: " + str(agentPos))
     elif movimento == 7:
-        if 0<=agentPos[0]-1<linha and 0<=agentPos[1]-1<coluna:
-            if grid[agentPos[0]-1][agentPos[1]-1] == '_':
+        if 0 <= agentPos[0] - 1 < linha and 0 <= agentPos[1] - 1 < coluna:
+            if grid[agentPos[0] - 1][agentPos[1] - 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] -= 1
                 agentPos[1] -= 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 8:
-        if 0<=agentPos[0]-1<linha and 0<=agentPos[1]<coluna:
-            if grid[agentPos[0]-1][agentPos[1]] == '_':
+        if 0 <= agentPos[0] - 1 < linha and 0 <= agentPos[1] < coluna:
+            if grid[agentPos[0] - 1][agentPos[1]] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] -= 1
-                #agentPos[1] -= 1
+                # agentPos[1] -= 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 9:
-        if 0<=agentPos[0]-1<linha and 0<=agentPos[1]+1<coluna:
-            if grid[agentPos[0]-1][agentPos[1]+1] == '_':
+        if 0 <= agentPos[0] - 1 < linha and 0 <= agentPos[1] + 1 < coluna:
+            if grid[agentPos[0] - 1][agentPos[1] + 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] -= 1
                 agentPos[1] += 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 4:
-        if 0<=agentPos[0]<linha and 0<=agentPos[1]-1<coluna:
-            if grid[agentPos[0]][agentPos[1]-1] == '_':
+        if 0 <= agentPos[0] < linha and 0 <= agentPos[1] - 1 < coluna:
+            if grid[agentPos[0]][agentPos[1] - 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
-                #agentPos[0] -= 1
+                # agentPos[0] -= 1
                 agentPos[1] -= 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 6:
-        if 0<=agentPos[0]<linha and 0<=agentPos[1]+1<coluna:
-            if grid[agentPos[0]][agentPos[1]+1] == '_':
+        if 0 <= agentPos[0] < linha and 0 <= agentPos[1] + 1 < coluna:
+            if grid[agentPos[0]][agentPos[1] + 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
-                #agentPos[0] -= 1
+                # agentPos[0] -= 1
                 agentPos[1] += 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 1:
-        if 0<=agentPos[0]+1<linha and 0<=agentPos[1]-1<coluna:
-            if grid[agentPos[0]+1][agentPos[1]-1] == '_':
+        if 0 <= agentPos[0] + 1 < linha and 0 <= agentPos[1] - 1 < coluna:
+            if grid[agentPos[0] + 1][agentPos[1] - 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] += 1
                 agentPos[1] -= 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 2:
-        if 0<=agentPos[0]+1<linha and 0<=agentPos[1]<coluna:
-            if grid[agentPos[0]+1][agentPos[1]] == '_':
+        if 0 <= agentPos[0] + 1 < linha and 0 <= agentPos[1] < coluna:
+            if grid[agentPos[0] + 1][agentPos[1]] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] += 1
-                #agentPos[1] -= 1
+                # agentPos[1] -= 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
     elif movimento == 3:
-        if 0<=agentPos[0]+1<linha and 0<=agentPos[1]+1<coluna:
-            if grid[agentPos[0]+1][agentPos[1]+1] == '_':
+        if 0 <= agentPos[0] + 1 < linha and 0 <= agentPos[1] + 1 < coluna:
+            if grid[agentPos[0] + 1][agentPos[1] + 1] == '_':
                 grid[agentPos[0]][agentPos[1]] = '_'
                 agentPos[0] += 1
                 agentPos[1] += 1
                 grid[agentPos[0]][agentPos[1]] = 'A'
 
     print_grid()
-
