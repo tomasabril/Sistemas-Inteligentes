abdca67ade48bff3c19c6b9a82d595106a1e9e58
tomasabril <dfhudf@falso.com>
2017-03-15 17:30:38 -0300

    acabou a aula

--- tarefa2/agente.py
+++ tarefa2/agente.py
@@ -10,3 +10,43 @@ class Agente():
         self.minhaPosicao = posicao_real
 
     def mover(self, direcao):
+        if movimento == 7:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] -= 1
+                    self.agentPos[1] -= 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 8:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] -= 1
+                    # agentPos[1] -= 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 9:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] -= 1
+                    self.agentPos[1] += 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 4:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    # self.agentPos[0] -= 1
+                    self.agentPos[1] -= 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 6:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    # self.agentPos[0] -= 1
+                    self.agentPos[1] += 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 1:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] += 1
+                    self.agentPos[1] -= 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 2:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] += 1
+                    # self.agentPos[1] -= 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
+        elif movimento == 3:
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = '_'
+                    self.agentPos[0] += 1
+                    self.agentPos[1] += 1
+                    self.grid[self.agentPos[0]][self.agentPos[1]] = 'A'
